5b5e3089a52ade3248d5b7338b4dbce164c7c17c
Daniyaal Beg <danyaalbeg@gmail.com>
2025-06-22 23:44:06 +0100

    fxi

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart({})],
-});
+	plugins: [
+		tsconfigPaths(),
+		tailwindcss(),
+		tanstackStart({
+			target: 'vercel',
+		}),
+	],
+})
